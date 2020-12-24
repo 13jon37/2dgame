305daed784514e7948ebf7bd6aee515c5a554700
13jon37 <62160236+13jon37@users.noreply.github.com>
2020-12-23 22:17:19 -0500

    comment message

--- win32_game/src/main.c
+++ win32_game/src/main.c
@@ -1,4 +1,21 @@
-// Push warning level to 3 
+/*
+* BIG thanks to Casey Muratori (Handmade Hero) - (https://handmadehero.org/) &
+* Ryan Ries on YouTube!!!!
+* The following code originates from both Casey Muratori's and Ryan Ries' YouTube video series. 
+* 
+*	*** THE ASSETS IN THIS CODEBASE BELONG ENTIRELY TO RYAN RIES. ***
+* 
+* I claim none of this code as my own, the brains behind it are theirs, though I do
+* plan to expand off into my own way of coding maybe using this as a base.
+* By that time the base code will probably have changed quite a bit, only then will I claim to 
+* be somewhat one of the brains behind the code, however, Ryan and Casey still are the main "sources" so 
+* to speak.
+* 
+* Casey Muratori: https://www.youtube.com/c/MollyRocket/
+* Ryan Ries:	  https://www.youtube.com/user/ryanries09
+*/
+
+// Push warning level to 3
 #pragma warning(push, 3)
 #include <stdio.h>
 #include <windows.h>
