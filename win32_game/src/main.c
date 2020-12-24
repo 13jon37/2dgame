b42ca3a9c762ba6851c800ec445419d5063b15af
13jon37 <62160236+13jon37@users.noreply.github.com>
2020-12-24 04:53:54 -0500

    yet another cleanup

--- win32_game/src/main.c
+++ win32_game/src/main.c
@@ -1,8 +1,6 @@
 /*
 * BIG thanks to Casey Muratori (Handmade Hero) - (https://handmadehero.org/) &
 * Ryan Ries on YouTube!!!!
-* The following code originates from both Casey Muratori's and Ryan Ries' YouTube video series. 
-* 
 * Casey Muratori: https://www.youtube.com/c/MollyRocket/
 * Ryan Ries:	  https://www.youtube.com/user/ryanries09
 */
