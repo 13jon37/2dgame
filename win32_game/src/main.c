430c92f0259abc612ecae02ec1f5acff432a7eb6
13jon37 <62160236+13jon37@users.noreply.github.com>
2020-12-24 04:50:30 -0500

    some removals

--- win32_game/src/main.c
+++ win32_game/src/main.c
@@ -3,9 +3,6 @@
 * Ryan Ries on YouTube!!!!
 * The following code originates from both Casey Muratori's and Ryan Ries' YouTube video series. 
 * 
-*	*** NOTE: SOME ASSETS IN THIS CODEBASE BELONG TO RYAN RIES. ***
-*	*** THE RED CHARACTER SPRITES ARE MADE BY ME.				***
-* 
 * I claim none of this code as my own, the brains behind it are theirs, though I do
 * plan to expand off into my own way of coding maybe using this as a base.
 * By that time the base code will probably have changed quite a bit, only then will I claim to 
